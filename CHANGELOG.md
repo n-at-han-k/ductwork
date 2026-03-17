f1ff740981a505ac630e3429a0338a9d7c5c7ac9
Tyler Ewing <tewing10@gmail.com>
2026-03-17 10:27:59 -0500

    fix: job availability claim and update state in a single transaction
    we also changed things around to query the whole availability record
    instead of just the ID, then doing another query with a join. this
    simplifies that a lil bit. the single transaction now protects against
    claiming a job, then an error happening, and the job claim being
    orphaned.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,7 @@
 
 ## [0.27.0] (Unreleased)
 
+- fix: job availability claim and update state in a single transaction
 - feat: complete and halt active branches for pipelines
 - feat: create initial branch when a pipeline is triggered
 - feat: introduce `Branch` and `BranchJunction` models and tables
--- lib/ductwork/row_locking_job_claim.rb
+++ lib/ductwork/row_locking_job_claim.rb
@@ -3,30 +3,32 @@
 module Ductwork
   class RowLockingJobClaim
     def initialize(klass)
-      @id = nil
+      @availability = nil
       @job = nil
       @klass = klass
       @process_id = ::Process.pid
     end
 
     def latest
-      rows_updated = attempt_availability_claim
+      Ductwork::Record.transaction do
+        claim_availability
 
-      if rows_updated == 1
-        Ductwork.logger.debug(
-          msg: "Job claimed",
-          role: :job_worker,
-          process_id: process_id,
-          availability_id: id
-        )
-        update_state
-      else
-        Ductwork.logger.debug(
-          msg: "No available job to claim",
-          role: :job_worker,
-          process_id: process_id,
-          pipeline: klass
-        )
+        if availability.present?
+          Ductwork.logger.debug(
+            msg: "Job claimed",
+            role: :job_worker,
+            process_id: process_id,
+            availability_id: availability.id
+          )
+          update_state
+        else
+          Ductwork.logger.debug(
+            msg: "No available job to claim",
+            role: :job_worker,
+            process_id: process_id,
+            pipeline: klass
+          )
+        end
       end
 
       job
@@ -34,42 +36,29 @@ module Ductwork
 
     private
 
-    attr_reader :id, :job, :klass, :process_id
+    attr_reader :availability, :job, :klass, :process_id
 
-    def attempt_availability_claim
-      Ductwork::Record.transaction do
-        @id = Ductwork::Availability
-              .where("ductwork_availabilities.started_at <= ?", Time.current)
-              .where(completed_at: nil, pipeline_klass: klass)
-              .order(:started_at)
-              .lock("FOR UPDATE SKIP LOCKED")
-              .limit(1)
-              .ids
-              .first
+    def claim_availability
+      @availability = Ductwork::Availability
+                      .where("ductwork_availabilities.started_at <= ?", Time.current)
+                      .where(completed_at: nil, pipeline_klass: klass)
+                      .order(:started_at)
+                      .lock("FOR UPDATE SKIP LOCKED")
+                      .limit(1)
+                      .first
 
-        if id.present?
-          Ductwork::Availability
-            .where(id: id, completed_at: nil)
-            .update_all(completed_at: Time.current, process_id: process_id)
-        else
-          0
-        end
-      end
+      return unless availability
+
+      availability.update_columns(completed_at: Time.current, process_id: process_id)
     end
 
     def update_state
-      Ductwork::Record.transaction do
-        execution = Ductwork::Execution
-                    .joins(:availability)
-                    .where(completed_at: nil)
-                    .where(ductwork_availabilities: { id: })
-                    .sole
-        @job = execution.job
+      execution = availability.execution
+      @job = execution.job
 
-        execution.update!(process_id:)
-        job.step.in_progress!
-        job.step.pipeline.in_progress!
-      end
+      execution.update!(process_id:)
+      job.step.in_progress!
+      job.step.pipeline.in_progress!
     end
   end
 end
